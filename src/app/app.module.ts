9189803da1c45f4315221f2fcc77ab5d6deb4f96
John Mota <johnmarcos.mota@gmail.com>
2023-03-22 20:50:28 -0300

    implementando serviço gerenciamento de moeda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConversorModule } from './conversor/conversor.module';
+import { MoedaModule } from './moeda/moeda.module';
+import { ConversaoModule } from './conversao/conversao.module';
+import { RespostaConversaoModule } from './resposta-conversao/resposta-conversao.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ConversorModule,
+    MoedaModule,
+    ConversaoModule,
+    RespostaConversaoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/conversor/conversor/conversor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conversor',
+  templateUrl: './conversor.component.html',
+  styleUrls: ['./conversor.component.css']
+})
+export class ConversorComponent {
+
+  private API_KEY: string = 'xaBM9gQSpONNRrdd1esErJ3PNyH8uz7e'
+
+}
